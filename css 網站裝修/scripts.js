e56281b621046a8380b20e8bb32a2440e745bf8b
william <user@users-MacBook-Air.local>
2017-12-17 14:19:04 -0800

    asdfasdfasdf

---
+++ css 網站裝修/scripts.js
@@ -0,0 +1,38 @@
+
+/*var food = prompt('你最愛吃什麼食物');
+if (food  >= '漢堡' ) {
+	console.log('good');
+}else if (food  >= '三明治' ){
+console.log('好吃');
+}
+ else {
+	console.log('bad')
+}
+for ( var number = 7; number > 0; number--){console.log(number);
+}*/
+
+
+/*var a =10;
+for (var i = 49; i > 0; i--) {
+    console.log('我有幾個蘋果： ' + i);
+}
+*/
+
+/*console.log("hello");
+
+for(var i = 15; i < 31; i++ ){
+	console.log("Students" + i);
+}
+
+function add(a, b) {
+	var c = a + b;
+	return c ;
+}*/
+
+$(document).ready(function(){
+ $("h1"). text("where to?");
+ $("h1").css('color','green');	
+$(".btn").click(function(){
+   $("h2").hide();
+});
+});
